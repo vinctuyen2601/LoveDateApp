f029ec9b62d9e4f80c1d5ba408b90a01c4e65887
mor-tuyennv <tuyen.nguyenvan@mor.com.vn>
2026-03-11 19:11:15 +0700

    fix ios build v2

--- plugins/withFirebasePodfileFix.js
+++ plugins/withFirebasePodfileFix.js
@@ -2,31 +2,36 @@ const { withDangerousMod } = require('@expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
-const PATCH_MARKER = 'ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES';
+const PATCH_MARKER = '# RNFB_PATCHED';
 
-const PATCH_BLOCK = `
-  # Fix: @react-native-firebase non-modular headers with use_frameworks! :static
+const PRE_INSTALL_BLOCK = `
+${PATCH_MARKER}
+# Force React-Native-Firebase pods to build as static libraries (not frameworks)
+# This avoids modular header import errors with use_frameworks! :static
+$RNFirebaseAsStaticFramework = true
+
+pre_install do |installer|
+  installer.pod_targets.each do |pod|
+    if pod.name.start_with?('RNFB')
+      def pod.build_type
+        Pod::BuildType.static_library
+      end
+    end
+  end
+end
+`;
+
+const POST_INSTALL_BLOCK = `
+  # Fix: @react-native-firebase non-modular headers
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |build_config|
       build_config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
-      build_config.build_settings['ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
-    end
-  end
 
-  # Also fix the main project build settings
-  installer.pods_project.build_configurations.each do |build_config|
-    build_config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
-    build_config.build_settings['ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
-  end
-
-  # Disable treating warnings as errors for RNFB pods
-  installer.pods_project.targets.each do |target|
-    if target.name.start_with?('RNFB') || target.name.start_with?('RNFBApp') || target.name.start_with?('RNFBAnalytics')
-      target.build_configurations.each do |build_config|
+      if target.name.start_with?('RNFB')
         build_config.build_settings['GCC_TREAT_WARNINGS_AS_ERRORS'] = 'NO'
         existing_flags = build_config.build_settings['OTHER_CFLAGS'] || '$(inherited)'
-        unless existing_flags.include?('-Wno-non-modular-include-in-framework-module')
-          build_config.build_settings['OTHER_CFLAGS'] = "#{existing_flags} -Wno-non-modular-include-in-framework-module"
+        unless existing_flags.include?('-Wno-error')
+          build_config.build_settings['OTHER_CFLAGS'] = "\#{existing_flags} -Wno-error -Wno-non-modular-include-in-framework-module"
         end
       end
     end
@@ -45,15 +50,25 @@ const withFirebasePodfileFix = (config) => {
         return config;
       }
 
-      // Strategy 1: inject after "post_install do |installer|" line
+      // Inject pre_install block before the first "target" declaration
+      if (contents.includes("target '")) {
+        contents = contents.replace(
+          /target '/,
+          `${PRE_INSTALL_BLOCK}\ntarget '`
+        );
+      } else {
+        // Fallback: prepend after use_frameworks line
+        contents = PRE_INSTALL_BLOCK + '\n' + contents;
+      }
+
+      // Inject post_install block
       if (contents.includes('post_install do |installer|')) {
         contents = contents.replace(
           'post_install do |installer|',
-          `post_install do |installer|\n${PATCH_BLOCK}`
+          `post_install do |installer|\n${POST_INSTALL_BLOCK}`
         );
       } else {
-        // Strategy 2: append a new post_install block at end of file
-        contents += `\npost_install do |installer|\n${PATCH_BLOCK}\nend\n`;
+        contents += `\npost_install do |installer|\n${POST_INSTALL_BLOCK}\nend\n`;
       }
 
       fs.writeFileSync(podfilePath, contents);
