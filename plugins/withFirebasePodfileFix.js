0044c8b0665a78742b0278473d6cac8a8189898e
mor-tuyennv <tuyen.nguyenvan@mor.com.vn>
2026-03-11 18:47:58 +0700

    fix ios build

--- plugins/withFirebasePodfileFix.js
+++ plugins/withFirebasePodfileFix.js
@@ -8,7 +8,27 @@ const PATCH_BLOCK = `
   # Fix: @react-native-firebase non-modular headers with use_frameworks! :static
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |build_config|
+      build_config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
       build_config.build_settings['ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+    end
+  end
+
+  # Also fix the main project build settings
+  installer.pods_project.build_configurations.each do |build_config|
+    build_config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+    build_config.build_settings['ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+  end
+
+  # Disable treating warnings as errors for RNFB pods
+  installer.pods_project.targets.each do |target|
+    if target.name.start_with?('RNFB') || target.name.start_with?('RNFBApp') || target.name.start_with?('RNFBAnalytics')
+      target.build_configurations.each do |build_config|
+        build_config.build_settings['GCC_TREAT_WARNINGS_AS_ERRORS'] = 'NO'
+        existing_flags = build_config.build_settings['OTHER_CFLAGS'] || '$(inherited)'
+        unless existing_flags.include?('-Wno-non-modular-include-in-framework-module')
+          build_config.build_settings['OTHER_CFLAGS'] = "#{existing_flags} -Wno-non-modular-include-in-framework-module"
+        end
+      end
     end
   end
 `;
